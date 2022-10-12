fe8dc2d85cb2dbf6924a8f7523465bbdcecc9510
Zaki nou <zakidenimes@hotmail.fr>
2022-10-12 13:15:03 +0200

    $attributs

--- classes/connexion.class.php
+++ classes/connexion.class.php
@@ -8,11 +8,10 @@
 
         // Identifiants pour la base de donnée SQL SERVER
        
-        $SQL_SERV="mysql:host=188.165.238.34;dbname=social-work-workshop;charset=utf8mb4";
-        $SQL_USERNAME="Admin";
+        $SQL_SERV="mysql:host=localhost;dbname=social-work-workshop;charset=utf8mb4";
+        $SQL_USERNAME="Admin2";
         $SQL_PASSWORD="WorkShop123";
-
-     
+        
         try
         {
             $connexion=new PDO ($SQL_SERV,$SQL_USERNAME,$SQL_PASSWORD);
