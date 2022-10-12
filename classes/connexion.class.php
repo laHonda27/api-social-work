0a1fecf152cadd4caadd61b8506bc9bd26a3a994
laHonda27 <88737633+laHonda27@users.noreply.github.com>
2022-10-12 13:57:33 +0200

    Update connexion.class.php

--- classes/connexion.class.php
+++ classes/connexion.class.php
@@ -9,8 +9,8 @@
         // Identifiants pour la base de donnée SQL SERVER
        
         $SQL_SERV="mysql:host=localhost;port=3306;dbname=social-work-workshop;charset=utf8mb4";
-        $SQL_USERNAME="root";
-        $SQL_PASSWORD="";
+        $SQL_USERNAME="Admin2";
+        $SQL_PASSWORD="WorkShop123";
         
         try
         {
