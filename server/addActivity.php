687624150f5c977e239866ef60199ea482214c37
Zaki nou <zakidenimes@hotmail.fr>
2022-10-13 15:31:20 +0200

    qa

--- server/addActivity.php
+++ server/addActivity.php
@@ -2,6 +2,9 @@
 
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Content-type: text/html; charset=UTF-8'); 
+header('Access-Control-Allow-Headers: X-Requested-With');
 
 require ('../classes/socialWork_gestion.class.php');
 
